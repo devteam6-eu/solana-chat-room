96a470955d8f39291a759ca83b6971edb169690f
devteam6-eu <omen.bondr@gmail.com>
2024-12-16 20:16:12 -0500

    Add files via upload

---
+++ tailwing.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      borderRadius: {
+        '3xl': '1.5rem',
+      },
+    },
+  },
+  plugins: [],
+}
